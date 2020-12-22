aa6d5f257cfbce6f26ec840ef9f1f717c1518f9b
Administrator <1746678613@qq.com>
2020-12-22 14:02:52 +0800

    bran1 --mybatis:添加部门表和增加外键约束

--- 001-test/src/main/java/com/zhang/sql/sql.sql
+++ 001-test/src/main/java/com/zhang/sql/sql.sql
@@ -5,3 +5,19 @@ CREATE TABLE tbl_employee(
 	email VARCHAR(255)
 
 )ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+
+
+CREATE TABLE tbl_department(
+   id int PRIMARY key auto_increment,
+	 dep_name VARCHAR(100)
+
+)ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+ALTER TABLE tbl_employee add COLUMN dp_id int(11);
+
+ALTER TABLE tbl_employee add CONSTRAINT fk Foreign KEY (dp_id) REFERENCES
+tbl_department(id)
+
+DESC tbl_employee;
