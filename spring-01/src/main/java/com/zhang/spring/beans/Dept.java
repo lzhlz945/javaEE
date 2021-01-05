9abdc25e6522cec5ab0dbd5790d182ef14d9196f
Administrator <1746678613@qq.com>
2021-01-05 17:18:45 +0800

    bran1 --spring5:级联注入和一些其他的注入

---
+++ spring-01/src/main/java/com/zhang/spring/beans/Dept.java
@@ -0,0 +1,14 @@
+package com.zhang.spring.beans;
+
+import lombok.Data;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: com.zhang.spring.beans
+ * @date:2021/1/5
+ */
+@Data
+public class Dept {
+    private String name;
+}
