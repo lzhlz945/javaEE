0572815034fc76adf266ef5746fa8b9dc2165ba5
Administrator <1746678613@qq.com>
2021-01-06 16:29:33 +0800

    bran1 --spring5:aop:相同切入点和切点表达式

--- spring-aop/src/test/java/com/zhang/aop/AppTest.java
+++ spring-aop/src/test/java/com/zhang/aop/AppTest.java
@@ -4,13 +4,17 @@ package com.zhang.aop;
 
 import static org.junit.Assert.assertTrue;
 
+import com.zhang.aop.bean.User;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    private ApplicationContext context=new ClassPathXmlApplicationContext("aspect01-spring.xml");
     /**
      * Rigorous Test :-)
      */
@@ -18,5 +22,12 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+
+    @Test
+    public void testBefor(){
+        User user = context.getBean("user", User.class);
+        user.add();
     }
 }
