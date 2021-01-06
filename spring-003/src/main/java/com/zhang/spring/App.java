80aae1a873696fff3b947a683093ccbf832ebc90
Administrator <1746678613@qq.com>
2021-01-06 09:26:45 +0800

    bran1 --spring5:bean的生命周期，初五步

---
+++ spring-003/src/main/java/com/zhang/spring/App.java
@@ -0,0 +1,17 @@
+package com.zhang.spring;
+
+import java.lang.annotation.Target;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
---
+++ spring-003/src/test/java/com/zhang/spring/AppTest.java
@@ -0,0 +1,36 @@
+package com.zhang.spring;
+
+import static org.junit.Assert.assertTrue;
+
+import com.zhang.spring.bean.TestBean;
+import lombok.Data;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+
+{
+    private ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("application-003.xml");
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+    @Test
+    public void test01(){
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println("第四步创建对象");
+        testBean.getName();
+        context.close();
+
+    }
+}
