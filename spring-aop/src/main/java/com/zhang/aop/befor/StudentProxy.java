40a86e1016a5165ff7a208047a51af3b9182b630
Administrator <1746678613@qq.com>
2021-01-06 16:55:05 +0800

    bran1 --spring5:aop:基于xml的aop

---
+++ spring-aop/src/main/java/com/zhang/aop/befor/StudentProxy.java
@@ -0,0 +1,13 @@
+package com.zhang.aop.befor;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: com.zhang.aop.befor
+ * @date:2021/1/6
+ */
+public class StudentProxy {
+    public void before(){
+        System.out.println("add之前....");
+    }
+}
--- spring-aop/src/test/java/com/zhang/aop/AppTest.java
+++ spring-aop/src/test/java/com/zhang/aop/AppTest.java
@@ -4,6 +4,7 @@ package com.zhang.aop;
 
 import static org.junit.Assert.assertTrue;
 
+import com.zhang.aop.bean.Student;
 import com.zhang.aop.bean.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -26,8 +27,26 @@ public class AppTest
 
 
     @Test
-    public void testBefor(){
+    public void testBeforan(){
         User user = context.getBean("user", User.class);
         user.add();
+    }
+
+    /**
+     * 基于xml的aop
+     *
+     */
+    @Test
+    public void testBeforxml(){
+        ApplicationContext context=new ClassPathXmlApplicationContext("aspect02-spring.xml");
+        Student student = context.getBean("student", Student.class);
+        student.add();
+    }
+
+    @Test
+    public void testBeforxml1(){
+        ApplicationContext context=new ClassPathXmlApplicationContext("aspect03xml-spring.xml");
+        Student student = context.getBean("student", Student.class);
+        student.add();
     }
 }
