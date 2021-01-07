22a582711ff1cffb0f67d182c43b9a4f4f17f25a
Administrator <1746678613@qq.com>
2021-01-07 15:53:52 +0800

    bran1 --spring5:spring-log-tx(完全注解)+log模块初始化

---
+++ spring-tx01/src/main/java/com/zhang/tx/config/SpringTx.java
@@ -0,0 +1,52 @@
+package com.zhang.tx.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: com.zhang.tx.config
+ * @date:2021/1/7
+ */
+@Configuration
+@ComponentScan(value = {"com.zhang.tx"})
+@EnableTransactionManagement
+public class SpringTx {
+    //创建数据库连接池
+    @Bean
+    public DruidDataSource getDruidDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mp?useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("888888");
+        return dataSource;
+    }
+
+    //创建JdbcTemplate对象
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+        //到ioc容器中根据类型找到dataSource
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        //注入dataSource
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+
+    //创建事务管理器
+//    @Bean
+//    public DataSourceTransactionManager getDataSourceTransactionManager(DataSource dataSource) {
+//        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+//        transactionManager.setDataSource(dataSource);
+//        return transactionManager;
+//    }
+
+
+}
