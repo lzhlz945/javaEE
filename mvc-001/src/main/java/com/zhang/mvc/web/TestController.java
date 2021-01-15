7a6fe9746932eab4f7ab10ee088e30f7d9fcf5c4
Administrator <1746678613@qq.com>
2021-01-15 10:48:04 +0800

    mvc 001:reset风格

--- mvc-001/src/main/java/com/zhang/mvc/web/TestController.java
+++ mvc-001/src/main/java/com/zhang/mvc/web/TestController.java
@@ -1,7 +1,9 @@
 package com.zhang.mvc.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author: create by zhl
@@ -15,6 +17,30 @@ public class TestController {
     @RequestMapping("/hello.do")
     public String test(){
 
+        return "success";
+    }
+
+    @RequestMapping(value = "/test/get/{id}",method = RequestMethod.GET)
+    public String test01(@PathVariable("id") Integer id){
+        System.out.println(id);
+        return "success";
+    }
+
+    @RequestMapping(value = "/test/post",method = RequestMethod.POST)
+    public String test01(){
+        System.out.println("post请求");
+        return "success";
+    }
+
+    @RequestMapping(value = "/test/delete/{id}",method = RequestMethod.DELETE)
+    public String test02(@PathVariable("id") Integer id){
+        System.out.println("delete请求"+id);
+        return "success";
+    }
+
+    @RequestMapping(value = "/test/put/{id}",method = RequestMethod.PUT)
+    public String test03(@PathVariable("id") Integer id){
+        System.out.println("put请求"+id);
         return "success";
     }
 }
--- mvc-001/src/main/webapp/index.jsp
+++ mvc-001/src/main/webapp/index.jsp
@@ -1,7 +1,25 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+    pageContext.setAttribute("Path",request.getContextPath());
+
+%>
 <html>
 <body>
 <h2>Hello World!</h2>
-<a href="hello.do">点击</a>
+<a href="${Path}/hello.do">点击</a>
+
+<a href="${Path}/test/get/1">get请求</a>
+
+<form action="test/post" method="post">
+    <input type="submit"  value="pos请求" />
+</form>
+    <form action="test/delete/1" method="post">
+       <input type="hidden" value="DELETE" name="_method"/>
+        <input type="submit" value="delete请求"/>
+    </form>
+        <form action="test/put/1" method="post">
+       <input type="hidden" value="PUT" name="_method"/>
+        <input type="submit" value="put请求"/>
+    </form>
 </body>
 </html>
