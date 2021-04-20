ffb019a59ef823ac17fa53688595c48bd86eb28f
Administrator <1746678613@qq.com>
2021-04-20 09:46:46 +0800

    mybatis plugin 实现interceptor 拦截目标方法，代理

--- 001-test/src/test/java/com/zhang/AppTest.java
+++ 001-test/src/test/java/com/zhang/AppTest.java
@@ -2,7 +2,15 @@ package com.zhang;
 
 import static org.junit.Assert.assertTrue;
 
+import com.zhang.mapper.EmployeeMapper1;
+import com.zhang.tx.bean.Employee;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+
+import java.io.InputStream;
 
 /**
  * Unit test for simple App.
@@ -16,5 +24,16 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void testPlugin() throws Exception{
+        String resources="mybatis-config.xml";
+        InputStream stream = Resources.getResourceAsStream(resources);
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(stream);
+        SqlSession sqlSession = factory.openSession();
+        EmployeeMapper1 mapper = sqlSession.getMapper(EmployeeMapper1.class);
+        Employee id = mapper.getId(1);
+        System.out.println(id);
+
     }
 }
--- zh-mybatis-spring-mvc/src/main/java/com/zhang/utils/TestBATH.java
+++ zh-mybatis-spring-mvc/src/main/java/com/zhang/utils/TestBATH.java
@@ -1,6 +1,14 @@
 package com.zhang.utils;
 
+import com.zhang.tx.bean.Employee;
+import com.zhang.tx.service.EmployeeService;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
 
 /**
  * @author: create by zhl
@@ -8,11 +16,21 @@ import org.junit.jupiter.api.Test;
  * @description: com.zhang.utils
  * @date:2020/12/28
  */
+@Controller
 public class TestBATH {
+
+    @Autowired
+    private SqlSessionTemplate template;
+    @Autowired
+    private  EmployeeService employeeService;
+
     @Test
     public void test01(){
 
 
+
+        List<Employee> emps = employeeService.getEmps();
+        System.out.println(emps);
 
 
     }
